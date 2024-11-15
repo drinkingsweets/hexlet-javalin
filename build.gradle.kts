172977eda8a824f400f77b0f092280efefb6bd5c
drinkingsweets <117858181+drinkingsweets@users.noreply.github.com>
2024-11-15 13:59:03 +0300

    7th lecture

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,13 +21,11 @@ dependencies {
     // Версии зависимостей могут отличаться
     // Здесь мы сразу подключаем зависимости,
     // которые понадобятся во время обучения
-    implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin:6.3.0")
     implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("io.javalin:javalin-rendering:6.1.3")
     implementation("gg.jte:jte:3.1.9")
     implementation("gg.jte:jte:3.1.9")
-    compileOnly("org.projectlombok:lombok:1.18.28")
-    annotationProcessor("org.projectlombok:lombok:1.18.28")
     implementation("io.javalin:javalin-rendering:6.1.3")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -3,9 +3,6 @@ package org.example.hexlet;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 
-import java.util.ArrayList;
-
-
 import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class HelloWorld {
@@ -14,26 +11,9 @@ public class HelloWorld {
             config.bundledPlugins.enableDevLogging();
             config.fileRenderer(new JavalinJte());
         });
-        ArrayList<Course> courses = new ArrayList<>();
 
-            courses.add(new Course(1, "course1", "it is course1"));
-            courses.add(new Course(2, "course2", "it is course2"));
-            courses.add(new Course(3, "course3", "it is course3"));
-            courses.add(new Course(4, "course4", "it is course4"));
-            courses.add(new Course(5, "course5", "it is course5"));
-        app.get("/courses", ctx -> {
-
-
-            var page = new CoursesPage(courses, "Курсы по программированию");
-            ctx.render("courses/index.jte", model("page", page));
-        });
-        app.get("/courses/{id}", ctx -> {
-            long id = ctx.pathParamAsClass("id", Long.class).get();
-            for(Course course: courses){
-                if (course.getId() == id){
-                    ctx.result("Course: " + course.getName() + "\n" + course.getDescription());
-                }
-            }
+        app.get("/", ctx -> {
+             ctx.render("index1.jte");
         });
 
         app.start(7070);
